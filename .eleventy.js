f3662d9b6780cdea0d52610773c7ffeae72c9390
Fábio Barbosa <hello@fabiobarbosa.net>
2020-06-29 01:39:23 +0200

    Try and fix PWA start_url offline bug

--- .eleventy.js
+++ .eleventy.js
@@ -29,7 +29,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginPWA, {
     modifyUrlPrefix: {
       "": "/"
-   }
+    }
   });
   eleventyConfig.addPlugin(plugini18n, {
     translations,
