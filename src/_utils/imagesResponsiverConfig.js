caae0df2191181036c0fbaf9ebb80af31292f68f
Fábio Barbosa <hello@fabiobarbosa.net>
2020-07-05 02:59:17 +0200

    Implement images with eleventy-plugin-images-responsiver

---
+++ src/_utils/imagesResponsiverConfig.js
@@ -0,0 +1,109 @@
+const site = require('../_data/metadata.json');
+const imageSize = require('image-size');
+const markdownIt = require('markdown-it');
+const md = new markdownIt();
+
+const runBeforeHook = (image, document) => {
+  // TODO: get "dist/" from config
+  let srcPath = './src/';
+
+  let imageSrc = image.getAttribute('src');
+  let imageUrl = '';
+
+  if (imageSrc.match(/^(https?:)?\/\//)) {
+    // TODO: find a way to get a remote image's dimensions
+    // TODO: some images are local but have an absolute URL
+    imageUrl = imageSrc;
+  } else {
+    let imageDimensions;
+    if (imageSrc[0] === '/') {
+      // TODO: get "src/" from Eleventy config
+      imageDimensions = imageSize('./src' + imageSrc);
+      imageUrl = site.url + imageSrc;
+    } else {
+      // This is a relative URL
+      imageDimensions = imageSize(srcPath + imageSrc);
+      imageUrl = site.url + imageSrc;
+    }
+    image.setAttribute('width', imageDimensions.width);
+    image.setAttribute('height', imageDimensions.height);
+    image.setAttribute('src', imageUrl);
+  }
+
+  image.dataset.responsiver = image.className;
+};
+
+const runAfterHook = (image, document) => {
+  let imageUrl =
+    image.getAttribute('data-pristine') || image.getAttribute('src');
+  let caption = image.getAttribute('title');
+
+  if (caption !== null) {
+    caption = md.renderInline(caption.trim());
+  }
+
+  let zoom = [...image.classList].indexOf('zoom') !== -1;
+
+  if (caption || zoom) {
+    const figure = document.createElement('figure');
+    figure.classList.add(...image.classList);
+    // TODO: decide weither classes should be removed from the image or not
+    image.classList.remove(...image.classList);
+    let figCaption = document.createElement('figcaption');
+    figCaption.innerHTML =
+      (caption ? caption : '') +
+      (zoom
+        ? `<p class="zoom">&#128269; See <a href="${imageUrl}">full size</a></p>`
+        : '');
+    figure.appendChild(image.cloneNode(true));
+    figure.appendChild(figCaption);
+
+    image.replaceWith(figure);
+  }
+};
+
+module.exports = {
+  default: {
+    selector: ':not(picture) img[src]:not([srcset]):not([src$=".svg"])',
+    resizedImageUrl: (src, width) =>
+      `https://res.cloudinary.com/fabiomrbarbosa/image/fetch/q_auto,f_auto,w_${width}/${src}`,
+    runBefore: runBeforeHook,
+    runAfter: runAfterHook,
+    fallbackWidth: 800,
+    minWidth: 360,
+    maxWidth: 1600,
+    sizes: '(max-width: 67rem) 90vw, 60rem',
+    attributes: {
+      loading: 'lazy',
+    },
+  },
+  twothirds: {
+    fallbackWidth: 600,
+    minWidth: 240,
+    maxWidth: 1120,
+    sizes: '(max-width: 20rem) 45vw, (max-width: 67rem) 60vw, 40rem',
+    classes: ['twothirds'],
+  },
+  onehalf: {
+    fallbackWidth: 400,
+    minWidth: 180,
+    maxWidth: 800,
+    sizes: '(max-width: 67rem) 45vw, 30rem',
+    classes: ['onehalf'],
+  },
+  onethird: {
+    fallbackWidth: 300,
+    minWidth: 120,
+    maxWidth: 560,
+    sizes: '(max-width: 20rem) 45vw, (max-width: 67rem) 30vw, 20rem',
+    classes: ['onethird', 'right'],
+  },
+  onefourth: {
+    fallbackWidth: 200,
+    minWidth: 100,
+    maxWidth: 400,
+    sizes:
+      '(max-width: 20rem) 45vw, (max-width: 30rem) 30vw, (max-width: 67rem) 22.5vw, 15rem',
+    classes: ['onefourth', 'right'],
+  },
+};
--- src/en/about.md
+++ src/en/about.md
@@ -10,6 +10,8 @@ eleventyNavigation:
   order: 1
 ---
 
+![My Headshot](assets/images/headshot.jpg){.onethird}
+
 I've always lived in between worlds: first as a native bilingual and an islander, born in the Azores, and now as a digital nomad. After dropping out of Journalism and Media Studies, I started my career as a podcaster and radio broadcaster. Five years later, I moved on to mainland Portugal, and thus to the world of marketing and advertising, where I had the chance to work as a freelance web developer, copy and scriptwriter, designer and English—Portuguese translator.
 
 In my work, I've come to develop the necessary expertise to deliver turn-key creative solutions for television and digital media. Nowadays, I intend to focus on sustainable, higher awareness projects that target structural change.
