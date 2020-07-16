9d7bfff8a7c458c8bcf6bbc696a66a8bc4e899cb
Fábio Barbosa <hello@fabiobarbosa.net>
2020-07-16 11:58:03 +0200

    Experiment with removing blog posts from the Workbox precache and adding fonts instead

--- workbox-config.js
+++ workbox-config.js
@@ -2,11 +2,18 @@ module.exports = {
   "cacheId": "fabiomrbarbosa-pwa",
   "globDirectory": "dist/",
   "globPatterns": [
-    "**/*.{html,svg,css,webmanifest}"
+    "**/*.{html,svg,css,webmanifest,ttf,woff2}"
+  ],
+  "globIgnores": [
+    "**/blog/**/*",
+    "**/tags/**/*",
+    "**/node_modules/**/*",
+    "sw.js",
+    "workbox-*.js"
   ],
   "runtimeCaching": [
     {
-      "urlPattern": "/^.*\.(webp|jpg|png|svg|html|ttf|woff2)$/",
+      "urlPattern": "/^.*\.(webp|jpg|png|svg|html|css|ttf|woff2)$/",
       "handler": "StaleWhileRevalidate"
     },
   ],
