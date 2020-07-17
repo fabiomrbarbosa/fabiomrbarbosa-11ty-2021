cfa3a1144cd6217076d86af6be89d28ac8af78a4
Fábio Barbosa <hello@fabiobarbosa.net>
2020-07-17 16:06:54 +0200

    Try and fix lack of auto-update in the PWA

--- workbox-config.js
+++ workbox-config.js
@@ -1,21 +1,18 @@
 module.exports = {
-  "cacheId": "fabiomrbarbosa-pwa",
-  "globDirectory": "dist/",
-  "globPatterns": [
-    "**/*.{html,svg,webmanifest}"
+  cacheId: "fabiomrbarbosa-pwa",
+  globDirectory: "dist/",
+  globPatterns: [
+    "**/*.{html,js,webmanifest}"
   ],
-  "globIgnores": [
+  globIgnores: [
     //"**/blog/**/*",
-    "**/tags/**/*",
-    "**/node_modules/**/*",
-    "sw.js",
-    "workbox-*.js"
+    "**/tags/**/*"
   ],
-  "runtimeCaching": [
+  runtimeCaching: [
     {
-      "urlPattern": "/^.*\.(webp|jpg|png|svg|html|css|ttf|woff2)$/",
-      "handler": "NetworkFirst"
+      urlPattern: "/^.*\.(webp|jpg|png|svg|html|css|ttf|woff2)$/",
+      handler: "StaleWhileRevalidate",
     },
   ],
-  "swDest": "dist/sw.js"
+  swDest: "dist/sw.js"
 };
