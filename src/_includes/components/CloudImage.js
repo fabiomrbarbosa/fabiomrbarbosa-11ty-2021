cfae05ba8fd9ed7667f9d09ded4d9ebe56c758a4
Fábio M.R. Barbosa <fabiomr.barbosa@gmail.com>
2021-09-17 13:16:46 +0200

    Update social media graph

--- src/_includes/components/CloudImage.js
+++ src/_includes/components/CloudImage.js
@@ -12,24 +12,24 @@ module.exports = function ({ title = "", description = "" } = {}) {
   let titleConfig = [
     "/w_740",
     "c_fit",
-    "co_rgb:fbfbfb",
+    "co_rgb:cd3700",
     "g_south_west",
     "x_480",
     "y_254",
-    "l_text:fonts:fmrb-title.ttf_86_letter_spacing_-2_line_spacing_-15:",
+    "l_text:fonts:crimson-roman-webfont.ttf_86_letter_spacing_-2_line_spacing_-15:",
   ].join(",");
   let titleUrl = fixedEncodeURIComponent(title);
   let taglineConfig = [
     "/w_740",
     "c_fit",
-    "co_rgb:fbfbfb",
+    "co_rgb:111111",
     "g_north_west",
     "x_480",
     "y_445",
-    "l_text:fonts:fmrb-tagline.ttf_56_letter_spacing_-1_line_spacing_-5:",
+    "l_text:fonts:crimson-roman-webfont.ttf_56_letter_spacing_-1_line_spacing_-5:",
   ].join(",");
   let taglineUrl = fixedEncodeURIComponent(description);
-  let imagePublicID = "/socialMediaCard";
+  let imagePublicID = "/socialMediaGraph";
 
   return (
     cloudName +
